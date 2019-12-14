ece6bf3ce31c06c757f2fec9ca1175b6399f1508
andres <luis.andradec14@gmail.com>
2019-12-14 09:37:57 -0500

    Commit create_subject.py and MateriasLoader.py

---
+++ MateriasLoaderToDB.py
@@ -0,0 +1,36 @@
+import MateriaClass
+import TareaClass
+import csv
+from trello import TrelloClient
+#f = open("calendar.csv","r")
+client = TrelloClient(api_key='',
+                      token='')
+all_boards = client.list_boards()
+last_board = all_boards[-2]
+id = ''
+try:
+    with open('materias2.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=';')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                # print(last_board.name)
+                for x in last_board.list_lists():
+                    if x.name == row[1]:
+                        id = x.id
+                        #print(x.name, x.id)
+                #my_list = last_board.get_list(id)
+                subject = MateriaClass.Materia(id, row[1], row[0])
+                print(subject.print())
+                sql = connectMySql.saveSubjects(subject)
+                #sql = connectMySql.exec("COMMIT;",connectMySql.getCur())
+                for row in sql.fetchall():
+                    print(row)
+                sql = connectMySql.getdb().close()
+except:
+    print("FALSE")
+#            line_count += 1
+#    print(f'Processed {line_count} lines.')
---
+++ create_subject.py
@@ -0,0 +1,32 @@
+import MateriaClass
+import TareaClass
+import csv
+from trello import TrelloClient
+import connectSQLite
+#f = open("calendar.csv","r")
+def create(subjCod):
+    client = TrelloClient(api_key='',
+                          token='')
+    all_boards = client.list_boards()
+    last_board = all_boards[-2]
+    id = ''
+    subjectsBoard = all_boards[-2]
+    if not subjectsBoard.id == "":
+        for x in all_boards:
+            if x.id == "":
+                subjectsBoard = x
+    else:
+        print("yes")
+
+    if(connectSQLite.check_subject_existence(subjCod) == 0):
+        subject_name = input("Se ha detectado una nueva materia, ingrese el nombre de la materia:")
+        subjectsBoard.add_list(subject_name)
+        for x in subjectsBoard.list_lists():
+            if x.name == row[1]:
+                id = x.id
+        subject = MateriaClass.Materia(id,subject_name,subjCod)
+        print(subject.print())
+        sql = connectSQLite.saveSubjects(subject)
+        for row in sql.fetchall():
+            print(row)
+        sql = connectSQLite.getdb().close()
