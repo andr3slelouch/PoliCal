571e20b9991c21551d712e91116d8c6c19ae972a
Carlos <41646603+reaperjason@users.noreply.github.com>
2020-02-08 19:43:28 -0500

    Update setup.py
    Codigo del setup
    Trabajo del grupo de: Arias Nicolas, Burbano Renato, Chicaiza Carlos, Vinueza Rafael  --terminado

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PoliCalAAL",
+    version="1.0.0",
+    author="Nicolas Arias",
+    author_email="jozuenikolas@gmail.com",
+    description="Este es un programa hecho en python para sincronizar tareas existentes en el aula virtual de moodle EPN con una cuenta de Trello.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andr3slelouch/PoliCal/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.2',
+)
