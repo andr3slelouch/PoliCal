e609e1473494d43253ba7b20ddabd64e5155a9ec
JORGE LUIS QUILUMBA TOAQUIZA <jorge.quilumba@epn.edu.ec>
2020-02-09 14:24:49 -0500

    Prueba unitaria acceso a Trello

--- PruebasUnitarias.py
+++ PruebasUnitarias.py
@@ -15,3 +15,18 @@ class TestAV(unittest.TestCase):
     
 if __name__ == "__main__":
     unittest.main()
+
+#test acceso a trello
+
+class TestTrel(unittest.TestCase):
+    def testTrello(self):
+        driver = webdriver.Chrome('C:\\drivers\\chromedriver_win32\\chromedriver.exe')
+        #driver = webdriver.Chrome(executable_path="C:\drivers\chromedriver_win32\chromedriver.exe")
+        driver.get('https://trello.com/es')
+
+        titleOfEwbPage=driver.title
+
+        self.assertTrue(titleOfEwbPage == "Trello")  #true      
+    
+if __name__ == "__main__":
+    unittest.main()
