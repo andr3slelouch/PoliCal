ecfb5739e384cb49d189016b69f055051e339cf7
Joel <k.joel2@hotmail.com>
2020-02-10 13:13:00 -0500

    modificacion funcion assertTrue

--- test_1.py
+++ test_1.py
@@ -3,6 +3,6 @@ import configuration
 
 class test_check_url(unittest.TestCase):
     def test_trello(self):
-        self.assertEqual(configuration.check_for_url('https://recentupcoming'),True)
+        self.assertTrue(configuration.check_for_url('https://recentupcoming'))
 
         
